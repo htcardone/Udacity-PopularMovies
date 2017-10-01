35e115a0a2fd21f877b042ddd447682f65d5752d
htcardone <htcardone@gmail.com>
2017-10-01 12:06:10 -0300

    Fix add/remove Favorite bug.

--- app/src/main/java/com/htcardone/popularmovies/moviedetail/MovieDetailPresenter.java
+++ app/src/main/java/com/htcardone/popularmovies/moviedetail/MovieDetailPresenter.java
@@ -53,10 +53,12 @@ public class MovieDetailPresenter implements MovieDetailContract.Presenter {
         if (mIsFavorite) {
             if (mMoviesRepository.unsetMovieAsFavorite(mMovie.getId())) {
                 mMovieDetailView.updateFavoriteButton(false);
+                mIsFavorite = false;
             }
         } else {
             if (mMoviesRepository.setMovieAsFavorite(mMovie.getId(), mMovie.getTitle())) {
                 mMovieDetailView.updateFavoriteButton(true);
+                mIsFavorite = true;
             }
         }
     }
